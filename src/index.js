8b8089272273cbf87d7475c9f02f1518124f4d5a
Junior Paula <luke.junnior@icloud.com>
2022-11-27 15:54:44 -0300

    generate bin of the code :)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
 #!/usr/bin/env node
+
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+
+import createFile from './createFiles.js'
+import createLayersIfNotExists from './createLayers.js'
+
+const { argv: { componentName } } = yargs(hideBin(process.argv))
+                .command('skeleton', 'create project skeleton', (builder) => {
+                    return builder
+                            .option('component-name', {
+                                alias: 'c',
+                                demandOption: true,
+                                describe: 'component\'s name',
+                                type: 'array'
+                            })
+                            .example('skeleton --component-name product', 'create a project with a single domain')
+                            .example('skeleton -c product -c person -c account', 'create a project with a list domain')
+                })
+                .epilog('copyright 2022 - Junior P Developer')
+
+const env = process.env.NODE_ENV
+const defaultMainFolder = env === 'dev' ? 'tmp' : 'src'
+
+const layers = ['repository', 'service', 'factory'].sort()
+const config = {
+    layers,
+    defaultMainFolder,
+    mainPath: '.'
+}
+
+await createLayersIfNotExists(config)
+
+const pendingPromises = []
+for (const domain of componentName) {
+    const result = createFile({
+        ...config,
+        componentName: domain
+    })
+    pendingPromises.push(result)
+}
+
+await Promise.all(pendingPromises)
